87181ae20d7a036a871c40b0449110273b40ed7d
Egor_Babcinetchi <egor.babcinetchi@isa.utm.md>
2022-09-07 21:57:41 +0300

    init basic project

---
+++ src/main/java/md/utm/isa/pr/lab1/consumer/ConsumerApplication.java
@@ -0,0 +1,13 @@
+package md.utm.isa.pr.lab1.consumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsumerApplication.class, args);
+	}
+
+}
